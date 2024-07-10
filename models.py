53a33ecb4f3e1691ffb3993ab18cb452be60b931
HiiipowerHiii <163337839+HiiipowerHiii@users.noreply.github.com>
2024-07-10 08:08:38 +0300

    improve variables

--- models.py
+++ models.py
@@ -5,16 +5,17 @@ import os
 load_dotenv()
 
 from flask import Flask
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
 
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    date = db.Column(db.Date, nullable=False)
-    time = db.Column(db.Time, nullable=False)
+event_notifier_app = Flask(__name__)
+event_notifier_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('EVENT_NOTIFIER_DATABASE_URL')
+database = SQLAlchemy(event_notifier_app)
 
-with app.app_context():
-    db.create_all()
+class Event(database.Model):
+    event_id = database.Column(database.Integer, primary_key=True)
+    event_title = database.Column(database.String(255), nullable=False)
+    event_description = database.Column(database.Text, nullable=True)
+    event_date = database.Column(database.Date, nullable=False)
+    event_time = database.Column(database.Time, nullable=False)
+
+with event_notifier_app.app_context():
+    database.create_all()
