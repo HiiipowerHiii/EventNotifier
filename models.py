909306e98bc714843a73fe498c826cf6c2a8a99b
HiiipowerHiii <163337839+HiiipowerHiii@users.noreply.github.com>
2024-07-12 13:12:33 +0300

    improve code readability

--- models.py
+++ models.py
@@ -1,16 +1,18 @@
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
-from flask import Flask
-
 event_notifier_app = Flask(__name__)
 event_notifier_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('EVENT_NOTIFIER_DATABASE_URL')
+
 database = SQLAlchemy(event_notifier_app)
 
 class Event(database.Model):
+    __tablename__ = 'events'
+
     event_id = database.Column(database.Integer, primary_key=True)
     event_title = database.Column(database.String(255), nullable=False)
     event_description = database.Column(database.Text, nullable=True)
