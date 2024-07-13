3e14e85fadf36d7a110eaa762bac7983ff62fa0e
HiiipowerHiii <163337839+HiiipowerHiii@users.noreply.github.com>
2024-07-13 16:03:06 +0300

    added new function

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
@@ -10,14 +11,25 @@ event_notifier_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('EVENT_NOT
 
 database = SQLAlchemy(event_notifier_app)
 
+logging.basicConfig(level=logging.INFO)
+
 class Event(database.Model):
     __tablename__ = 'events'
-
     event_id = database.Column(database.Integer, primary_key=True)
     event_title = database.Column(database.String(255), nullable=False)
     event_description = database.Column(database.Text, nullable=True)
     event_date = database.Column(database.Date, nullable=False)
     event_time = database.Column(database.Time, nullable=False)
 
+def log_events():
+    events = Event.query.all()
+    if events:
+        for event in events:
+            logging.info(f'Event ID: {event.event_id}, Title: {event.event_title}, Date: {event.event_
+date}, Time: {event.event_time}')
+    else:
+        logging.info('No events found in the database.')
+
 with event_notifier_app.app_context():
     database.create_all()
+    log_events()
