a7c300216870d8681d55fd3b594db50621182b20
HiiipowerHiii <163337839+HiiipowerHiii@users.noreply.github.com>
2024-07-20 00:56:48 +0300

    change names

--- db.py
+++ db.py
@@ -1,13 +1,23 @@
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+
+# Load environment variables
 load_dotenv()
-DATABASE_USER = os.environ.get('DATABASE_USER')
-DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
-DATABASE_HOST = os.environ.get('DATABASE_HOST')
-DATABASE_PORT = os.environ.get('DATABASE_PORT')
-DATABASE_NAME = os.environ.get('DATABASE_NAME')
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
-engine = create_engine(DATABASE_CONNECTION_URI)
-def get_db_connection():
-    return engine.connect()
+
+# Database Configuration Variables
+DB_USER = os.environ.get('DATABASE_USER')
+DB_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+DB_HOST = os.environ.get('DATABASE_HOST')
+DB_PORT = os.environ.get('DATABASE_PORT')
+DB_NAME = os.environ.get('DATABASE_NAME')
+
+# Construct Database Connection URI
+DB_CONNECTION_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+# Initialize Database Engine
+db_engine = create_engine(DB_CONNECTION_URI)
+
+def establish_db_connection():
+    """Establish and return a connection to the database."""
+    return db_engine.connect()
